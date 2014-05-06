4d456c37fb165eb12e9efde5a73784f8329e5967
Sphoorti Joglekar <sphoorti.joglekar@gmail.com>
2014-05-06 22:20:21 +0530

    Python Script

--- nasa_background.py
+++ nasa_background.py
@@ -1,15 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib2, re, datetime, os
 
-#URL used to fetch the background image
 url='http://apod.nasa.gov/apod/'
 response=urllib2.urlopen(url)
 content=response.read()
 response.close()
-
 soup = BeautifulSoup(content)
-
 url=url+str(re.search(r'"([A-Za-z0-9_\./\\-]*)"', str(soup.find_all('a')[1])).group())[1:-1]
+#print url
 response=urllib2.urlopen(url)
 store = open ( str(datetime.date.today())+".jpg" , 'w')
 store.write ( response.read())
@@ -17,4 +15,5 @@ store.close()
 response.close()
 
 #Change this line with respect to the Desktop Environment
-os.system("feh --bg-fill " + str(datetime.date.today())+".jpg")
+path = os.getcwd()
+os.system("gsettings set org.cinnamon.desktop.background picture-uri file://" + path + "/" + str(datetime.date.today())+".jpg")
